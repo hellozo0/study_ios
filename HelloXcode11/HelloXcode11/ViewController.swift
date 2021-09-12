50bbef878d6b92e0599578c2e7c934eb04e0a7ba
안현주 <ahju0011@gmail.com>
2021-09-12 23:04:48 +0900

    ch5,6 xcode11부터 달라진것들

---
+++ HelloXcode11/HelloXcode11/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  HelloXcode11
+//
+//  Created by 안현주 on 2021/09/12.
+//
+
+import UIKit
+
+
+class ViewController : UIViewController{
+    override func viewDidLoad(){
+        super.viewDidLoad()
+    }
+}
